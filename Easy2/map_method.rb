be0fc20575f091022b7bd3ce232e62c62bc9a69b
Kyle McCurley <kmccurley123@gmail.com>
2020-03-12 11:56:03 -0400

    Create map_method.rb

---
+++ Easy2/map_method.rb
@@ -0,0 +1,16 @@
+=begin
+  Input: 1x Array
+  Output: New Array with elements containing block return values
+=end
+
+def map(arr)
+  output = []
+  arr.each { |el| output << yield(el) if block_given? }
+  output
+end
+
+p map([1, 3, 6]) { |value| value**2 } == [1, 9, 36]
+p map([]) { |value| true } == []
+p map(['a', 'b', 'c', 'd']) { |value| false } == [false, false, false, false]
+p map(['a', 'b', 'c', 'd']) { |value| value.upcase } == ['A', 'B', 'C', 'D']
+p map([1, 3, 4]) { |value| (1..value).to_a } == [[1], [1, 2, 3], [1, 2, 3, 4]]
