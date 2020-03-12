a9e2bca7aba30592f15f8bb8f92c736ff5cacb8a
Kyle McCurley <kmccurley123@gmail.com>
2020-03-12 12:02:04 -0400

    Create count_method.rb

---
+++ Easy2/count_method.rb
@@ -0,0 +1,12 @@
+def count(*nums)
+  count = 0
+  nums.each { |num| count += 1 if yield(num) }
+  count
+end
+
+puts count(1, 3, 6) { |value| value.odd? } == 2
+puts count(1, 3, 6) { |value| value.even? } == 1
+puts count(1, 3, 6) { |value| value > 6 } == 0
+puts count(1, 3, 6) { |value| true } == 3
+puts count() { |value| true } == 0
+puts count(1, 3, 6) { |value| value - 6 } == 3
