3fefb06436b78c0216b1d649a456c320f0f62297
Kyle McCurley <kmccurley123@gmail.com>
2020-03-16 21:40:46 -0400

    Update pass_params_part_2.rb

--- Medium1/pass_params_part_2.rb
+++ Medium1/pass_params_part_2.rb
@@ -1,3 +1,9 @@
+# Write a method that takes an array as an argument.
+# The method should yield the contents of the array to a block,
+# which should assign your block variables 
+# in such a way that it ignores the first two elements, 
+# and groups all remaining elements as a raptors array.
+
 def pass_block(arr)
   yield arr
 end
