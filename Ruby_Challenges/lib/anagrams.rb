00e4ee19d0a9e952a9dc99e69ce6ec9470226c92
Kyle McCurley <kmccurley123@gmail.com>
2020-03-20 11:39:27 -0400

    Update anagrams.rb

--- Ruby_Challenges/lib/anagrams.rb
+++ Ruby_Challenges/lib/anagrams.rb
@@ -1,1 +1,51 @@
-# Placeholder File
+=begin
+Write a program that, given a word and a list of possible anagrams,
+selects the correct sublist that contains the anagrams of the word.
+
+For example, given the word "listen" and a list of candidates like "enlists" 
+"google" "inlets" "banana" the program should return a list containing "inlets".
+Please read the test suite for exact rules of anagrams.
+
+Objective:
+  Select the correct sublist that contains the anagrams of a word
+  Input: String, Array of possible anagrams as strings
+  
+Algorithm for detecting an anagram:
+  - Initialize an empty array: anagrams -> []
+  - Iterate through each string of anagram_candidates: |candidate|
+    - Sort the String#chars of candidate
+    - If the sorted candidate is the same as the sorted control_word
+      - Append candidate to the anagrams array
+      
+  SET anagrams = []
+  LOOP anagram_candidates do |candidate|
+    IF candidate.chars.sort == @control.chars.sort
+      APPEND candidate to anagrams
+  RETURN anagrams
+=end
+
+class Anagram
+  def initialize(control_word)
+    @control = control_word
+  end
+  
+  def match(anagram_candidates)
+    anagrams = find_all_anagrams(anagram_candidates)
+    anagrams
+  end
+  
+  private
+  
+  def find_all_anagrams(candidates)
+    outcome = []
+    candidates.each do |candidate|
+      outcome << candidate if anagram?(candidate)
+    end
+    outcome
+  end
+  
+  def anagram?(candidate)
+    candidate.downcase.chars.sort == @control.downcase.chars.sort &&
+    candidate.downcase != @control.downcase
+  end
+end
