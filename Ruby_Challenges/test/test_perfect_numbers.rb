7b0097f3d68cef8b1e084aa04ae0748f0c5e455e
Kyle McCurley <kmccurley123@gmail.com>
2020-03-21 23:40:56 -0400

    Update test_perfect_number.rb

--- Ruby_Challenges/test/test_perfect_numbers.rb
+++ Ruby_Challenges/test/test_perfect_numbers.rb
@@ -1,43 +1,22 @@
-=begin
-'Meta-View' of The Program:
-  - Calculate the factors of the given number
-  - Calculate the sum of the factors
-  - Based on the sum of the factors:
-    - Decide if:
-      - Perfect
-      - Abundant
-      - Deficient
-      
-  Calculate the Factors of The Given Number:
-    - Initialize a local variable: outcome
-      - Set to an empty array
-    - Iterate from 1 upto (given_number - 1): |factor|
-      - If num % factor == 0 (If this factor is in fact a factor):
-        - Append the factor to outcome
-    - Based on the sum of the factors:
-      - If sum == given number:
-        - Return 'perfect'
-      - If the sum > number:
-        - Return 'abundant'
-      - If the sum < number:
-        - Return 'deficient'
-=end
+require 'minitest/autorun'
+require_relative '../lib/perfect_numbers'
 
-class PerfectNumber
-  def self.classify(number)
-    raise RuntimeError if number < 0
-    factors = [] # First, Calculate the factors of @number:
-    1.upto(number - 1) do |factor|
-      factors << factor if number % factor == 0
+class PerfectNumberTest < Minitest::Test
+  def test_initialize_perfect_number
+    assert_raises RuntimeError do
+      PerfectNumber.classify(-1)
     end
-    sum_of_factors = factors.reduce(:+) # Find the sum of the factors
-    # Categorize/Return the number:
-    if sum_of_factors < number
-      'deficient'
-    elsif sum_of_factors > number
-      'abundant'
-    else
-      'perfect'
-    end
+  end
+
+  def test_classify_deficient
+    assert_equal 'deficient', PerfectNumber.classify(13)
+  end
+
+  def test_classify_perfect
+    assert_equal 'perfect', PerfectNumber.classify(28)
+  end
+
+  def test_classify_abundant
+    assert_equal 'abundant', PerfectNumber.classify(12)
   end
 end
